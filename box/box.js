ea0bf5bfdc282c6c54f299c282efeb2db03c6dc9
venkat080396 <102867591+venkat080396@users.noreply.github.com>
2022-05-17 16:06:50 +0100

    Add files via upload

--- box/box.js
+++ box/box.js
@@ -1,80 +1,124 @@
 (function() { 
 	let template = document.createElement("template");
 	template.innerHTML = `
-    <style>
-    :canvas {
-        -moz-user-select: none;
-        -webkit-user-select: none;
-        -ms-user-select: none;
-      }
-    </style>
+		<style>
+		:host {
+			border-radius: 10px;
+			border-width: 2px;
+			border-color: black;
+			border-style: solid;
+			display: block;
+		} 
 
-        <div id="container" style="width: 50%;">
-            <canvas id="canvas"></canvas>
-        </div>
+		body {
+		  background: #fff;
+		}
+		
+		.metric {
+		  padding: 10%;
+		}
+		
+		.metric svg {
+		  max-width: 100%;
+		}
+		
+		.metric path {
+		  stroke-width: 75;
+		  stroke: #ecf0f1;
+		  fill: none;
+		}
+		
+		.metric text {
+		  font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;
+		}
+		
+		.metric.participation path.data-arc {
+		  stroke: #27ae60;
+		}
+		
+		.metric.participation text {
+		  fill: #27ae60;
+		}		
+		</style>
+		
+		<div class="container">
+		  <div class="row">
+		    <div class="col-md-4 col-sm-4">
+		      <div class="metric participation" data-ratio=".95">
+		        <svg viewBox="0 0 1000 500">
+			        <path d="M 950 500 A 450 450 0 0 0 50 500"></path>
+					<text class='percentage' text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">0%</text>
+					<text class='title' text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal"></text>
+  	            </svg>
+		      </div>
+		    </div>
+		  </div>
+		</div>
 	`;
 
-	function randomValues(count, min, max) {
-        const delta = max - min;
-        return Array.from({length: count}).map(() => Math.random() * delta + min);
-      }
-      
-      const boxplotData = {
-        // define label tree
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-          label: 'Dataset 1',
-          backgroundColor: 'rgba(255,0,0,0.5)',
-          borderColor: 'red',
-          borderWidth: 1,
-          outlierColor: '#999999',
-          padding: 10,
-          itemRadius: 0,
-          data: [
-            randomValues(100, 0, 100),
-            randomValues(100, 0, 20),
-            randomValues(100, 20, 70),
-            randomValues(100, 60, 100),
-            randomValues(40, 50, 100),
-            randomValues(100, 60, 120),
-            randomValues(100, 80, 100)
-          ]
-        }, {
-          label: 'Dataset 2',
-          backgroundColor:  'rgba(0,0,255,0.5)',
-          borderColor: 'blue',
-          borderWidth: 1,
-          outlierColor: '#999999',
-          padding: 10,
-          itemRadius: 0,
-          data: [
-            randomValues(100, 60, 100),
-            randomValues(100, 0, 100),
-            randomValues(100, 0, 20),
-            randomValues(100, 20, 70),
-            randomValues(40, 60, 120),
-            randomValues(100, 20, 100),
-            randomValues(100, 80, 100)
-          ]
-        }]
-      };
-      window.onload = () => {
-        const ctx = document.getElementById("canvas").getContext("2d");
-        window.myBar = new Chart(ctx, {
-          type: 'boxplot',
-          data: boxplotData,
-          options: {
-            responsive: true,
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: true,
-              text: 'Chart.js Box Plot Chart'
-            }
-          }
-        });
-        
-      };
-      customElements.define("com-demo-gauge", Box);
-    })();
+	class Box extends HTMLElement {
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			
+			this.$style = shadowRoot.querySelector('style');			
+			this.$svg = shadowRoot.querySelector('svg');
+			
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
+			
+			this._props = {};
+		}
+		
+		render(val, info, color) {
+			var val1 = val * 0.01;
+			var x = this.svg_circle_arc_path(500, 500, 450, -90, val1 * 180.0 - 90);
+			var rounded = Math.round( val * 10 ) / 10;
+
+			
+			if(rounded >=0 && rounded <=100) {
+				this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: black;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 75;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-arc {stroke: ' + color + ';}.metric.participation text {fill: ' + color + ';}';
+				this.$svg.innerHTML = '<path d="M 950 500 A 450 450 0 0 0 50 500"></path><text class="percentage" text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">' + rounded + '%</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal">' + info + '</text><path d="' + x + '" class="data-arc"></path>"';
+			}
+		}
+		  
+		polar_to_cartesian(cx, cy, radius, angle) {
+		    var radians;
+		    radians = (angle - 90) * Math.PI / 180.0;
+		    return [Math.round((cx + radius * Math.cos(radians)) * 100) / 100, Math.round((cy + radius * Math.sin(radians)) * 100) / 100];
+		}
+		
+		svg_circle_arc_path(x, y, radius, start_angle, end_angle) {
+		    var end_xy, start_xy;
+		    start_xy = this.polar_to_cartesian(x, y, radius, end_angle);
+		    end_xy = this.polar_to_cartesian(x, y, radius, start_angle);
+		    return "M " + start_xy[0] + " " + start_xy[1] + " A " + radius + " " + radius + " 0 0 0 " + end_xy[0] + " " + end_xy[1];
+		  };
+		  
+
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = { ...this._props, ...changedProperties };
+		}
+
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("value" in changedProperties) {
+				this.$value = changedProperties["value"];
+			}
+			
+			if ("info" in changedProperties) {
+				this.$info = changedProperties["info"];
+			}
+			
+			if ("color" in changedProperties) {
+				this.$color = changedProperties["color"];
+			}
+			
+			this.render(this.$value, this.$info, this.$color);
+		}
+	}
+	
+	customElements.define("com-demo-gauge", Box);
+})();
